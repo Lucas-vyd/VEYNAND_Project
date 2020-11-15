d7069beb45909e476b54b34162d4fd7268d04155
Lucas Veynand <lucas.veynand@gmail.com>
2020-11-15 18:59:27 +0100

    delete

--- Java/ProgObject/Les-gardien-de-la-galaxie/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"application","l":"JeuShoot"},{"p":"application","l":"Monstre"},{"p":"application","l":"Tir"},{"p":"application","l":"Vaisseau"}]
--- Java/ProgObject/Les-gardien-de-la-galaxie/src/graphisme/TestImage.java
+++
@@ -1,8 +0,0 @@
-package graphisme;
-
-public class TestImage {
-	
-	public static void main(String[] args) {
-		Sprite.getAlien();
-	}
-}
